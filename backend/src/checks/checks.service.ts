ba7dab7d86a257e86a5fcfb8bc17cb31e890dd3b
Paulo Mariano <paulomariano2501@gmail.com>
2023-02-25 03:56:57 -0300

    dayjs and worked hours working

--- backend/src/checks/checks.service.ts
+++ backend/src/checks/checks.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateCheckDto } from './dto/create-check.dto';
 import { UpdateCheckDto } from './dto/update-check.dto';
+import * as dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 
 @Injectable()
 export class ChecksService {
@@ -9,6 +11,17 @@ export class ChecksService {
   constructor(private prisma: PrismaService) { }
 
   async create(data: CreateCheckDto) {
+
+    const alreadyCheckedToday = await this.prisma.check.findFirst({
+      where: {
+        entryTime: {
+          equals: data.entryTime
+        }
+      }
+    })
+
+    if (alreadyCheckedToday)
+      return "You already check in today!"
 
     const payload = await this.prisma.check.create({
       data
@@ -26,11 +39,29 @@ export class ChecksService {
 
   async update(id: number, updateCheckDto: UpdateCheckDto) {
 
+    const { entryTime } = await this.prisma.check.findFirst({
+      where: { id },
+      select: {
+        entryTime: true
+      }
+    })
+
+    const departureTime = dayjs(updateCheckDto.departureTime)
+
+    const duration = dayjs(departureTime).diff(dayjs(entryTime), 'seconds')
+
+    const time = new Date(1970, 0 , 1);
+   
+    time.setSeconds(duration);
+
+    const workedHours = dayjs(time).subtract(3, 'hours').toDate()
+
     const payload = await this.prisma.check.update({
       where: { id },
       data: {
         ...updateCheckDto,
-        isWorking: false
+        isWorking: false,
+        workedHours
       }
     })
 
