feb37e1d4da4c425b613fc41329f8eb091d91112
Paulo Mariano <paulomariano2501@gmail.com>
2023-02-25 00:29:16 -0300

    create user with randomstring to code

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,14 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as randomstring from 'randomstring';
 
 @Injectable()
 export class UsersService {
 
   constructor(private prisma: PrismaService) { }
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+
+    const code = randomstring.generate(8);
+
+    const payload = await this.prisma.user.create({
+
+      data: {
+        ...createUserDto,
+        code
+      }
+
+    })
+
+    return payload;
   }
 
   async findAll() {
@@ -19,15 +32,15 @@ export class UsersService {
     return payload;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+ async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
