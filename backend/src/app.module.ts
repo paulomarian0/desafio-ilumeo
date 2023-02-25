c76c0a2bf1bc9e35076290aa619e0f73f996b7f1
Paulo Mariano <paulomariano2501@gmail.com>
2023-02-25 01:03:10 -0300

    jwt auth complete

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaModule } from 'prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
-import { ChecksModule } from './checks/checks.module';
 
 @Module({
-  imports: [UsersModule, ChecksModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
